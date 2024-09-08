8008caefc6b6433d43749437bacdf512e913be47
suhaibghori <ghorisuhaib09@gmail.com>
2024-09-08 16:06:02 +0500

    all update

---
+++ app.js
@@ -0,0 +1,222 @@
+                                            // PRACTICE QUESTIONS STARTS......
+                                        
+                                        
+                                        
+            //CH#01 STARTS
+        // Q#01
+        // alert("Welcome Visitors..");
+                                        
+        // Q#02
+        // alert("Error!Please enter a valid Password")
+                                        
+        // Q#03
+        // alert("Welcome to Js Land ..\n Happy Coding!")
+                                        
+        // Q#04
+        // alert("Welcome To js Land")
+        // alert("Happy Coding \n Prevent this from page extra dialogue")
+                                        
+        // Q#05
+        // console.log("Hello .... I can Run JS through my Web Broswer Console")
+                                        
+        // CH#01 ENDS
+                                        
+                                        
+                                        
+        //  CH#02 STARTS
+                                        
+        // Q#01
+        // var username;
+                                        
+        // Q#02
+        // var username ="myName"
+        // myName="Suhaib Ghori"
+                                        
+        // Q#03
+        // PART#a
+        // var name ="message";
+                                        
+        //PART#b
+        //   message ="Hello World";
+                                        
+        //PART#c
+        // alert(message);
+                                        
+                                        
+        // Q#04
+        // var name="SUHAIB GHORI";
+        // var age="17 years old";
+        // var about="Certified Mobile Application Development";
+        // alert(name);
+        // alert(age);
+        // alert(about);
+                                        
+        // Q#05
+        // alert("PIZZA \n PIZZ \n PIZ \n Pi \n P");
+                                        
+        // Q#06
+        //  var email="example@example.com"
+        //  alert("My email address is" + " " + email)
+                                        
+        // Q#07
+        // var book="I am tried to learn from the book"+" "+book; 
+        // alert(book);
+                                        
+        // Q#08
+        //  document.write("Yah! I can write HTML through JavaScript");
+                                        
+        // Q#09
+        // var design="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+        // alert(design);
+                                        
+        // CH#02 ENDS
+                                        
+                                        
+                                        
+                                        
+        // ch#03 STARTS
+                                        
+        // Q#01
+                                        
+        // var age; 
+        // age ="18 years old"
+        // alert("I am"+age);
+                                        
+        // Q#02
+        // var time;
+        // time="You have visited this site 14 times";
+        // alert(time);
+                                        
+        // Q#03
+        // var birthYear="My birthyear is 2007.";
+        // document.write("<br>"+birthYear+"<br> Data type of my declared variable is number.");
+                                        
+        // Q#04
+        // PART#a
+        //  var visitorName = "John Doe"; 
+        // PART#b
+        //  var productTitle = "T-shirt";
+        // PART#c 
+        //  var quantity = 5;  
+        //  var message =visitorName+" "+"ordered"+" "+quantity+" "+productTitle+"(s) on XYZ Clothing store.";
+        //  document.write(message);
+                                        
+        // CH#03 ENDS
+                                        
+                                        
+                                        
+                                        
+        // CH#04 STARTS
+                                        
+        // Q#01
+        //  var name="John Doe", productTitle = "T-shirt", quantity = 5;
+                                        
+        // Q#02
+        // Legal & Illegal Variable Names.
+        // Legal Variables:
+                                        
+        //  1. userName:  
+        //  2. _age:  
+        //  3. $price:  
+        //  4. totalAmount:  
+        //  5. user1:
+                                        
+        //  Illegal Variable 
+        // 1. 1user:
+        // 2. user-name: 
+        // 3. user name: 
+        // 4. @username:
+        // 5. var:
+                                        
+                                        
+        //Q#03
+                                        
+         // PART#a
+        //   document.write("<h1>Rules for naming JS variables</h1>");
+         //  PART#b
+        //  document.write("<p>Variable names can only contain , letters, numbers,$, and _.For example: $my_1stVariable.</p>");
+        //  PART#c
+        //  document.write("<p>Variables must begin with a letter,$ or _. For example $name, _name or name.</p>");
+         //  PART#d
+        //   document.write("<p>Variable names are case sensitive.</p>");
+         //  PART#e
+        //  document.write("<p>Variable names should not be JS keywords.</p>");
+                                        
+        //  CH#04 ENDS
+                                        
+        // PRACTICE QUESTIONS ENDS......
+                                        
+                                        
+                        // ASSIGNMEMT QUESTIONS STARTS.....
+        // CH#01 (ALERTS)
+        // Q#01
+                                        
+        // Part#01
+                                        
+        // alert("SUHAIB");
+                                        
+        // PART#02:
+        // alert("GHORI");
+                                        
+        // PART#03:
+        // alert("example@example.com");
+                                        
+        // PART#04:
+        // alert("0317*****78");
+                                        
+        // PART#05:
+        // alert("**********");
+                                        
+        // Q#02
+                                        
+        // alert("You're learning JavaScript!");
+                                        
+        // Q#03
+                                        
+        // alert("Hello, Welcome to our Website!");
+                                        
+        // CH#02 (Variables for string)
+                                        
+        // Q#01
+        // var userAge =30;
+                                        
+        // Q#02
+        // var message;
+        // message ="Hello Developers";
+                                        
+        // Q#03
+        // var teamName ="Dreamteam";
+        // alert(teamName);
+                                        
+        // Q#04
+        // var bestMan ="Charlie";
+        // bestMan="Babbage";
+                                        
+        // CH#03(Variables for numbers)
+        // Q#01
+        // var caseQty;
+                                        
+         // Q#02
+        // caseQty = 144;
+                                        
+        // Q#03
+        // caseQty= "9"
+                                        
+        // Q#04
+        // var num1=15;
+        // var num2=15;
+        // var result=(num1+num2);
+                                        
+        // Q#05
+        //  var merchTotal = 100;
+        //  var shippingCharge = 10;
+        //  var orderTotal = (100 + 10);
+        //  document.write(orderTotal);
+                                        
+        // Q:6
+        // var merchTotal = 500;
+        // var shippingCharge = -230;
+        // var orderTotal = 500+(-230);
+        // document.write(orderTotal);
+                                        
+        // ASSIGNMENT QUESTIONS ENDS
